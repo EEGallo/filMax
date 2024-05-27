d01702aa32f3e32ecee1467275d3c7ad7af6c395
Emanuel Gallo <emanuelgallodg@gmail.com>
2024-05-27 02:35:23 -0300

    reestructuración + JS

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+const array = ['PRIMERO', 'SEGUNDO', 'TERCERO','CUARTO'];
+
+function mostrarEnConsola(elemento, index, array) {
+    console.log(elemento);
+}
+
+array.forEach(insertCard)
+
+function insertCard(movie, index, movies) {
+    
+}
